6a4734660b86a7ff6f48122b77a4229d81540d47
HellsingDarge <20254450+HellsingDarge@users.noreply.github.com>
2020-05-23 12:43:57 +0700

    Added database and moved data to there

---
+++ src/main/resources/db/migration/V0__Initialisation.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Departments(
+    name varchar(64),
+    phoneNumber varchar (16),
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE Employees(
+    name varchar(64),
+    department varchar(64) NOT NULL,
+    externalNumber varchar(16),
+    internalNumber char(3),
+    homeNumber varchar(16),
+    PRIMARY KEY (name),
+    FOREIGN KEY (department) REFERENCES Departments(name)
+);
---
+++ src/main/resources/db/migration/V1__Populate.sql
@@ -0,0 +1,13 @@
+INSERT INTO Departments VALUES
+('Division 66', '123456789'),
+('Division 79', '456789132'),
+('Division 42', '321654897'),
+('SET', '753869421'),
+('Dispatch', '159487263'),
+('ONIGRU', null);
+
+INSERT INTO Employees VALUES
+('Anashel', 'Division 66', null, null, null),
+('Aphelion', 'Division 42', null, '111', null),
+('Miller', 'Dispatch', '55555555', '453', '78945632111'),
+('Samantha', 'Division 79', '77777777', '777', null);
